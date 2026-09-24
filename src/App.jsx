d93aafd3aff523d3bccffe0b941d3a0839bee830
Thorix Rover <thorixrover@gmail.com>
2026-09-24 09:23:55 +0900

    feat: initialize project structure with core components and styles
    - Added main application file (App.jsx) and entry point (main.jsx).
    - Implemented Navbar component for navigation.
    - Created Cube component with GSAP animations and 3D rendering.
    - Defined constants for navigation links, client reviews, and projects.
    - Established global styles in index.css with Tailwind CSS and custom themes.
    - Integrated Vite for development and build processes.
    - Added SVG logo for branding.
    - Set up Hero section with placeholders for future content.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./sections/navbar"
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,25 @@
+import { Leva } from 'leva';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useMediaQuery } from 'react-responsive';
+import { PerspectiveCamera } from '@react-three/drei';
+
+import Cube from '../components/Cube.js';
+import Rings from '../components/Rings.jsx';
+import ReactLogo from '../components/ReactLogo.jsx';
+import Button from '../components/Button.jsx';
+import Target from '../components/Target.jsx';
+import CanvasLoader from '../components/Loading.jsx';
+import HeroCamera from '../components/HeroCamera.jsx';
+import { calculateSizes } from '../constants/index.js';
+import { HackerRoom } from '../components/HackerRoom.jsx';
+
+const Hero = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hero
